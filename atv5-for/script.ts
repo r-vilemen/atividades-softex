565ea37308c7e4b0e9e3658294066a18262674a1
r-vilemen <rodrigo.vilemen@hotmail.com>
2024-08-01 16:19:54 -0300

    add questions

---
+++ atv5-for/script.ts
@@ -0,0 +1,29 @@
+// Questão 1
+
+// É uma estrutura de repetição de um bloco de código
+
+// Questão 2
+
+// Através de uma declaração inicial
+
+// Questão 3
+
+// Quando a iteração do loop (bloco de código) for falsa.
+
+// Questão 4
+
+// É controlada pela expressão de incremento (++) ou decremento (--).
+
+// Questão 5
+
+for (let i = 0; i < 10; i++) {
+  console.log(`testando uma frase! ${i}`);
+}
+
+// Questão 6
+
+for (let i2 = 0; i2 < 10; i2++) {
+  console.log(i2);
+}
+
+// Questão 7
