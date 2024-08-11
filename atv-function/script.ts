0dbf5f0101e3821ef6cf602bb3898acd40289b30
r-vilemen <rodrigo.vilemen@hotmail.com>
2024-08-11 13:15:33 -0300

    atividades em sala

---
+++ atv-function/script.ts
@@ -0,0 +1,24 @@
+// function criarFrases() {
+//   const frases = ["A vida é bela", "O amor é lindo", "A amizade é tudo"];
+
+//   frases.forEach((frase) => console.log(frase));
+// }
+
+// criarFrases();
+
+// function soma(valor1: any, valor2: any) {
+//   const resultado = valor1 + valor2;
+//   console.log(resultado);
+// }
+
+// soma(1, 2);
+
+// function mostrarFraseNoConsole(texto: string) {
+//   console.log(texto);
+// }
+
+// mostrarFraseNoConsole("Olá, mundo!");
+
+function soma2(valor1: number, valor2: number, frase?: string) {
+  return valor1 + valor2;
+}
